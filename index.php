b848c186f9e89a803925115acc9909995ee1251b
Tim Usenbenz <tim@usnbnz.de>
2024-10-30 13:47:21 +0100

    feat(index): TailwindCSS test

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 // Startseite
 // Suchleiste, Neue Posts, Link zu Profil/Login/Registrieren
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible"
+        content="IE=edge">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline bg-slate-500 p-5 text-blue-300">
+        Tailwindcss using Play CDN
+    </h1>
+    <script src="https://cdn.tailwindcss.com"></script>
+</body>
+</html>
