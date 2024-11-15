de87861648cd93b294d2975015206197a6cdaf32
Tim U <tim@usnbnz.de>
2024-11-15 14:09:46 +0100

    fix(suchleiste): added tailwindcss

--- html/suchleiste.php
+++ html/suchleiste.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tailwind Navbar mit Suchleiste</title>
-    <link rel="stylesheet" href="output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100">
 
